699dde9c902422bb080b80090d0d6d05d3a09fb2
kk6 <hiro.ashiya@gmail.com>
2019-04-11 02:44:42 +0900

    プログレスバーも動くようにした

--- download.py
+++ download.py
@@ -28,30 +28,29 @@ def fetch_isbn_list():
 async def fetch_book_data(session, isbn_list, sem):
     isbn_csv = ",".join(isbn_list)
     url = f"https://api.openbd.jp/v1/get?isbn={isbn_csv}"
-    with await sem:
+    async with sem:
         async with session.get(url) as res:
-            return await res.text()
+            return await res.json()
 
 
 async def fetch_all(chunked_isbn_list, limit=5):
     sem = asyncio.Semaphore(limit)
     async with aiohttp.ClientSession() as session:
-        for isbn_list in chunked_isbn_list:
-            data = await fetch_book_data(session, isbn_list, sem)
-            print(data)
+        cors = [fetch_book_data(session, isbn_list, sem) for isbn_list in chunked_isbn_list]
+        responses = [await f for f in tqdm(asyncio.as_completed(cors), total=len(cors))]
+        return responses
 
 
 async def main():
     all_isbn_list = fetch_isbn_list()
-    books_total = len(all_isbn_list)
 
     chunked_isbn_list = more_itertools.chunked(all_isbn_list, 1000)
-
-    with tqdm(total=books_total) as progress_bar:
-        progress_bar.set_description("Downloading book summary")
-        return await fetch_all(chunked_isbn_list)
+    return await fetch_all(chunked_isbn_list)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    res = loop.run_until_complete(main())
+    for rs in res:
+        for r in rs:
+            print(r["summary"]["isbn"])
